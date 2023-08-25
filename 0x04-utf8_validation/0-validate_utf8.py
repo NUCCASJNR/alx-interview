d4a7448ec01abc4cc4f48e46a2e97051850c866f
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-25 09:21:07 +0100

    more updates

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,12 +9,10 @@ def validUTF8(data: List[int]) -> Union[bool, List[str]]:
     """
     Validates Utf-8
     """
-    new_li = []
     for char in data:
         if char <= 255:
             try:
                 decoded_char = bytes([char]).decode("utf-8")
-                new_li.append(decoded_char)
             except UnicodeDecodeError:
                 return False
         else:
