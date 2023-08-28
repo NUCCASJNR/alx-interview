032fcf0db790e1f5d703a3d976b9e09e231e41a6
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-28 01:28:56 +0100

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,16 +5,38 @@
 from typing import List, Union
 
 
-def validUTF8(data: List[int]) -> Union[bool, List[str]]:
+# def validUTF8(data: List[int]) -> Union[bool, List[str]]:
+#     """
+#     Validates Utf-8
+#     """
+#     for char in data:
+#         if char in range(0, 256):
+#             try:
+#                 decoded_char = bytes([char]).decode("utf-8")
+#             except IndexError and UnicodeError:
+#                 return False
+#         else:
+#             return False
+#     return True
+
+def validUTF8(data) -> bool:
     """
-    Validates Utf-8
+    Returns True if data is a valid UTF-8 encoding, else return False
+    :param data:
     """
-    for char in data:
-        if char in range(0, 256):
-            try:
-                decoded_char = bytes([char]).decode("utf-8")
-            except IndexError and UnicodeError:
+    num_bytes = 0
+    for byte in data:
+        mask = 1 << 7
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            return False
-    return True
+            if byte >> 6 != 0b10:
+                return False
+            num_bytes -= 1
+    return num_bytes == 0
