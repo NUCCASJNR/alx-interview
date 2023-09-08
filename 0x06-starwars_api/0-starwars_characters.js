761476c73b0c13f0a135b27be40387da5528e50b
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-08 18:07:49 +0100

    starwars async

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -9,52 +9,27 @@ function getRequest (url) {
       } else if (response.statusCode !== 200) {
         reject(new Error(`Request failed with status code ${response.statusCode}`));
       } else {
-        try {
-          const movieData = JSON.parse(body);
-          const chars = movieData.characters;
-          const characterPromises = chars.map(fetchCharacter);
-          Promise.all(characterPromises)
-            .then(characterNames => {
-              resolve(characterNames);
-            })
-            .catch(reject);
-        } catch (parseError) {
-          reject(parseError);
-        }
+        resolve(JSON.parse(body));
       }
     });
   });
 }
 
-function fetchCharacter (characterUrl) {
-  return new Promise((resolve, reject) => {
-    req.get(characterUrl, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else if (response.statusCode !== 200) {
-        reject(new Error(`Request failed with status code ${response.statusCode}`));
-      } else {
-        try {
-          const characterData = JSON.parse(body);
-          resolve(characterData.name);
-        } catch (parseError) {
-          reject(parseError);
-        }
-      }
-    });
-  });
-}
+async function fetchMovieAndCharacters (movieId) {
+  try {
+    const movieData = await getRequest(`https://swapi-api.alx-tools.com/api/films/${movieId}`);
+    const characterPromises = movieData.characters.map(characterUrl =>
+      getRequest(characterUrl).then(characterData => characterData.name)
+    );
 
-const MovieId = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films';
-const CompleteUrl = `${url}/${MovieId}`;
-
-getRequest(CompleteUrl)
-  .then(characterNames => {
+    const characterNames = await Promise.all(characterPromises);
     characterNames.forEach(characterName => {
-      console.log(characterName);
+      console.log(`${characterName}`);
     });
-  })
-  .catch(error => {
+  } catch (error) {
     console.error(`Error: ${error}`);
-  });
+  }
+}
+
+const movieId = process.argv[2];
+fetchMovieAndCharacters(movieId);
