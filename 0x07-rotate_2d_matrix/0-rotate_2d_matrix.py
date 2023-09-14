19afeecf872a77482d3e7d63084fd534bb69f18e
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-14 12:19:58 +0100

    rotate 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Contains a function that rotates a matrix 90 degrees clockwise"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate 2d matrix
+    Args:
+        matrix:
+        matrix
+    Returns:
+        Rotated matrix
+    """
+
+    duplicated_matrix = [row[:] for row in matrix]
+    matrix_size = len(matrix)
+    b = -1
+    # iterate through each row in the duplicated matrix
+    for row in duplicated_matrix:
+        # Iterate through each column in the original matrix
+        for a in range(matrix_size):
+            # update the values of the original matrix with the values
+            # from the copied matrix
+            matrix[a][b] = row[a]
+        # Move to the previous column for the next iteration
+        b -= 1
