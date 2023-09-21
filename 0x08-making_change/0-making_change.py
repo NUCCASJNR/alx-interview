24352863ca651fce4e10c028570ea6c1ea9f291f
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-21 14:17:17 +0100

    Add docs

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -16,12 +16,19 @@ def makeChange(coins: List[int], total: int) -> int:
     """
     if total <= 0:
         return 0
+    """Sort the coins in descending order"""
     sorted_coins = sorted(coins, reverse=True)
     total_coins_used = 0
+    """Loop through the sorted coins"""
     for coin in sorted_coins:
+        """while the total is greater or equals to the current coin"""
         while total >= coin:
+            """Subtract the current coin from the total"""
             total -= coin
+            """Increment the total coins used by 1"""
             total_coins_used += 1
+    """if the total == 0 that means the subtraction was successful"""
     if total == 0:
+        """return the total coins used"""
         return total_coins_used
     return -1
